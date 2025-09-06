3bc55b15e6d0c9e4db4f34e1b331afbc839f9b7f
Timothy Hoffpauir <timothy.hoffpauir@icloud.com>
2025-09-06 15:58:14 -0500

    Delete tests directory

--- tests/test_imports.py
+++
@@ -1,7 +0,0 @@
-# tests/test_imports.py
-import importlib
-
-
-def test_import_eliteparser():
-    """Elite Parser core module should import without side-effects or crashes."""
-    importlib.import_module("eliteparser")
